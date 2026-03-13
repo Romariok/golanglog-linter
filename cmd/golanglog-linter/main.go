3178fb467302485acc9047734a20f432e2f63261
Romariok <ronya-ko@yandex.ru>
2026-03-13 15:53:57 +0300

    golangci-lint plugin integration, CI/CD, and documentation

--- cmd/golanglog-linter/main.go
+++ cmd/golanglog-linter/main.go
@@ -1,3 +1,10 @@
 package main
 
-func main() {}
+import (
+	"github.com/romariok/golanglog-linter/pkg/golanglog"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+func main() {
+	singlechecker.Main(golanglog.Analyzer)
+}
--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,3 +1,19 @@
+//go:build ignore
+
+// Plugin is the golangci-lint module plugin entry point.
+// Build with: go build -buildmode=plugin -o golanglog.so ./plugin/
 package main
 
-func main() {}
+import (
+	"github.com/romariok/golanglog-linter/pkg/golanglog"
+	"golang.org/x/tools/go/analysis"
+)
+
+type analyzerPlugin struct{}
+
+func (*analyzerPlugin) GetAnalyzers() []*analysis.Analyzer {
+	return []*analysis.Analyzer{golanglog.Analyzer}
+}
+
+// AnalyzerPlugin is the exported plugin symbol for golangci-lint module plugin.
+var AnalyzerPlugin analyzerPlugin
